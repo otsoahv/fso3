fca70c541aa40d5d0e17a98eb114e58da2d1adf8
Otso Ahvonen <otso.ahvonen@gmail.com>
2023-02-09 22:26:51 +0200

    osa3 3.6 valmis

--- index.js
+++ index.js
@@ -1,41 +1,106 @@
+const http = require('http')
 const express = require('express')
 const app = express()
 
-let notes = [
+app.use(express.json())
+
+let persons = [
   {
-    "persons": [
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "qwer",
-        "key": "qwer",
-        "number": "1234",
-        "id": 4
-      },
-      {
-        "name": "qwerr",
-        "key": "qwerr",
-        "number": "12345",
-        "id": 5
-      }
-    ]
+    "name": "Otso Ahvonene",
+    "number": "123456-67890",
+    "id": 1
+  },{
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "qwer",
+    "key": "qwer",
+    "number": "1234",
+    "id": 4
+  },
+  {
+    "name": "qwerr",
+    "key": "qwerr",
+    "number": "12345",
+    "id": 5
   }
 ]
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10000)
+}
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const currentTime = new Date().toString();
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${currentTime}</p>`
+  )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = persons.find(note => note.id === id)
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  const duplicate = persons.find(person => person.name === body.name)
+  if (duplicate) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number || false,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
